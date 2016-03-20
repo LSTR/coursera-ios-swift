2748443dd5378305e08368248142fd35ea25d6eb
Lester Narvasta Ramirez <lesternr@gmail.com>
2016-03-20 10:49:04 -0500

    Velocimetro

--- coursera-ios-swift/Coursera-Swift.playground/Contents.swift
+++ coursera-ios-swift/Coursera-Swift.playground/Contents.swift
@@ -2,19 +2,50 @@
 
 import UIKit
 
-//* Juego De Memoria
+//* Un velocímetro de un automóvil digital
 
-for i in 0...100 {
-    if i%5==0 {
-        print("# \(i) Bingo!!!")
+public enum Velocidades:Int{
+    case Apagado = 0
+    case VelocidadBaja = 20
+    case VelocidadMedia = 50
+    case VelocidadAlta = 120
+    
+    init(velocidadInicial: Velocidades){
+        self = velocidadInicial
     }
-    if i%2==0 {
-        print("# \(i) Par!!!")
+}
+
+
+public class Auto{
+    var velocidad:Velocidades
+    
+    init(){
+        velocidad = Velocidades.Apagado
     }
-    if i%2==1 {
-        print("# \(i) Impar!!!")
+    
+    func cambioVelocidad() -> (actual:Int,velocidadEnCadena:String){
+        var cadena = ""
+        let vActual = velocidad.rawValue
+        if(velocidad==Velocidades.Apagado){
+            velocidad = Velocidades.VelocidadBaja
+            cadena = "Apagado"
+        }else if(velocidad==Velocidades.VelocidadBaja){
+            velocidad = Velocidades.VelocidadMedia
+            cadena = "Velocidad baja"
+        }else if(velocidad==Velocidades.VelocidadMedia){
+            velocidad = Velocidades.VelocidadAlta
+            cadena = "Velocidad media"
+        }else if(velocidad==Velocidades.VelocidadAlta){
+            velocidad = Velocidades.VelocidadMedia
+            cadena = "Velocidad alta"
+        }
+        return (vActual,cadena)
     }
-    if i >= 30 && i <= 40 {
-        print("# \(i) Viva Swift!!!")
-    }
+}
+
+var auto = Auto()
+
+for i in 1...20 {
+    let r = auto.cambioVelocidad()
+    print("\(i). \(r.actual), \(r.velocidadEnCadena)")
 }
